48ee5ba27416a8c36c2b3829fc1b14037c9e9e1d
leviestudante689-boop <leviestudante689@gmail.com>
2026-05-15 17:27:58 -0300

    Variaveis globais do dados,py
    Variáveis globais: lista principal, fila, pilha, tuplas de status e prioridade

--- dados.py
+++ dados.py
@@ -1,1 +1,16 @@
+#Variavel gloval para armazenar os dados
 
+lista_principal = [] #a lista é a lista basicamente dos alunos e tals, nao tem mt que falar nao
+
+fifo_atendimento = [] # a fila em fifo pra controla a ordem de atendimento dos alunos, ou seja, o primeiro a chegar é o primeiro a ser atendido
+
+lifo_sessoes = [] # a pilha em lifo para controlar as sessoes, ou seja, a ultima sessao a ser criada é a primeira a ser atendida!!!!!!!!!
+
+#agora é as tuplas 
+
+status_aluno = ("ativo", "inativo", "pendente") 
+
+status_prioridade = ("alta", "media", "baixa")
+
+#Aqui foi usado as listas para armazenar dados, sendo a dados.py onde ta as variveis globais
+#alem disso usamos lifo, fifo e tuplas pra organiza tudo direitnho e nao haver colapso do sistema
